32734a9ffa21eaac8872bff11c6221a9c20cbdde
leearaneta <lee.araneta@gmail.com>
2016-07-25 15:00:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {addBump, getBumps}
+}
+
+const {addBump, getBumps} = bumpCounter()
+
+function createAnimal(animal) {
+  function deadlyDevice(device) {
+    var obj = {}
+    obj.animalType = animal
+    obj.deadlyDevice = device
+    return obj
+  }
+  return deadlyDevice
+}
+
+sharkCreator = createAnimal('Shark')
+sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+sharkWithFrickinCannon = sharkCreator('Cannon')
